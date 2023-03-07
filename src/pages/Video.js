f7b175afc8b1d7e1dc0db2eb5072f80f64a5e136
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2023-03-07 21:39:40 +0600

    Set up react router dom

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -7,8 +7,6 @@ import Navbar from "../components/navbar/Navbar";
 export default function Video() {
     return (
         <>
-            <Navbar />
-
             <section class="pt-6 pb-20">
                 <div class="mx-auto max-w-7xl px-2 pb-20 min-h-[400px]">
                     <div class="grid grid-cols-3 gap-2 lg:gap-8">
@@ -22,8 +20,6 @@ export default function Video() {
                     </div>
                 </div>
             </section>
-
-            <Footer />
         </>
     );
 }
